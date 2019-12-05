bbf7d81fa2d24c9717bdeff623e6a29c58959ab9
0x384c0 <0x384c0@gmail.com>
2019-12-05 12:05:41 +0600

    updated CI scripts

--- .travis/linux-mingw/build.sh
+++ .travis/linux-mingw/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -ex
 mkdir "$HOME/.ccache" || true
 docker run --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache yuzuemu/build-environments:linux-mingw /bin/bash -ex /yuzu/.travis/linux-mingw/docker.sh
+
+mkdir -p artifacts
+tar -czf artifacts/ccache.tar.gz $HOME/.ccache
